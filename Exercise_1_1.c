e952294b404f1aa67043ac40c5326875dd9eb540
Cameron-Grey-Kunstadt <cameron.kunstadt@gmail.com>
2022-12-08 22:22:44 -0700

    Add files via upload

---
+++ Exercise_1_1.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Basic IO library for C
+
+void main(void){
+    
+    printf("Hello, world\n");
+}
---
+++ Exercise_1_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Basic IO library for C
+
+void main(void){
+    
+    printf("Hello, \cworld\n");
+}
+
+// The \c causes an "unknown escape sequence" error
